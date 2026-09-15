e296a48f0b85227e15023af9799c1adb7d0f53dd
Ravi Ranjan Mishra <013.raviranjan@gmail.com>
2026-09-15 15:57:12 +0530

    email correction

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -9,7 +9,7 @@ router.post(
   '/register',
   [
     body('name').trim().notEmpty().withMessage('Name is required'),
-    body('email').isEmail().withMessage('Enter a valid email').normalizeEmail(),
+    body('email').isEmail().withMessage('Enter a valid email'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   ],
   register
@@ -18,7 +18,7 @@ router.post(
 router.post(
   '/login',
   [
-    body('email').isEmail().withMessage('Enter a valid email').normalizeEmail(),
+    body('email').isEmail().withMessage('Enter a valid email'),
     body('password').notEmpty().withMessage('Password is required'),
   ],
   login
